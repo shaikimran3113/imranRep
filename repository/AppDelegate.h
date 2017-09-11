99fbc44ac0e5445399df57fa7710f1a7ac4d7708
student <student@Achievers-iMac.local>
2017-09-11 11:37:15 +0530

    Initial Commit

---
+++ repository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  repository
+//
+//  Created by student on 9/11/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ repository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  repository
+//
+//  Created by student on 9/11/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
